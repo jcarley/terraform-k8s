b824ca866c4f148dc91c199d47f5efb52c2e99ba
Jeff Carley <jeff@datica.com>
2018-03-14 10:09:47 -0500

    Adding the cerificates generation resource

---
+++ aws/certificates.tf
@@ -0,0 +1,38 @@
+resource "null_resource" "certificates" {
+  # triggers {
+  #   template_rendered = "${ data.template_file.certificates.rendered }"
+  # }
+  # provisioner "local-exec" {
+  #   command = "echo '${ data.template_file.certificates.rendered }' > ../cert/kubernetes-csr.json"
+  # }
+
+  provisioner "local-exec" {
+    command = "cd certs; rm *.pem; rm *.csr"
+  }
+
+  provisioner "local-exec" {
+    command = "cd certs; cfssl gencert -initca ca-csr.json | cfssljson -bare ca"
+  }
+
+  provisioner "local-exec" {
+    command = "cd certs; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes admin-csr.json | cfssljson -bare admin"
+  }
+
+  provisioner "local-exec" {
+    command = "cd certs; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kube-proxy-csr.json | cfssljson -bare kube-proxy"
+  }
+
+  # provisioner "local-exec" {
+  #   command = "cd certs; "
+  # }
+
+# cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,kubernetes.default -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes
+}
+
+
+# 10.32.0.1
+# 10.240.0.10
+# 10.240.0.11
+# 10.240.0.12
+
+
